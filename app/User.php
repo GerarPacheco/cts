e9cb7310c40433a7b4395f131416eb0527bedbf4
Gerardo Pacheco <gpacheco@ingebits.com>
2018-06-23 14:04:20 -0400

    Adding mutator for password

--- app/User.php
+++ app/User.php
@@ -9,21 +9,13 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $guarded = ['id'];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
+
+    public function setPasswordAttribute($value){
+        if (!$value ==NULL) {
+            $this->attributes['password'] = bcrypt($value);
+        }
+    }
 }
