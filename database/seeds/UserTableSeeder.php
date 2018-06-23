6e6567e0965a1ab02889c1d0f0c5e7e8964dbdd3
Gerardo Pacheco <gpacheco@ingebits.com>
2018-06-23 14:06:31 -0400

    Adding sedeer user

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder {
+
+	public function run()
+	{
+		//DB::table('users')->delete();
+
+		// Gerardo
+		$gerardo=User::create(array(
+			'name' => 'Gerardo Pacheco',
+			'email' => 'gpacheco@ingebits.com',
+			'password' => '123456',
+		));
+		//$gerardo->roles()->sync([1]);
+	
+	}
+}
