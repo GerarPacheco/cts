621a9e190b52c8cf922350660768d0fed4da81b4
Gerardo Pacheco <gpacheco@ingebits.com>
2018-06-23 14:56:32 -0400

    Adding Entrust model user

--- app/User.php
+++ app/User.php
@@ -4,10 +4,14 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use SoftDeletes;
+    use EntrustUserTrait {EntrustUserTrait::restore insteadof SoftDeletes;}
 
     protected $guarded = ['id'];
 
